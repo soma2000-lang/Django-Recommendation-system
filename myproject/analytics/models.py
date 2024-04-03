eca8ad37a117b21ef8b485178d880652f8ea1a3d
Somasree Majumder <bishnupadamajumder32@gmail.com>
2024-04-03 23:19:47 +0530

    adding the apps

--- myproject/analytics/models.py
+++ myproject/analytics/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+class Ratings(models.Model):
+    userId=models.CharField(max_length=16)
+    rating=models.DecimalField(max_length=2)
+    rating_timestamp=models.DateTimeField()
+    time= models.CharField(max_length=8, default='explicit')
+    
+    def __str__(self) -> str:
+        return "user_id: {}, movie_id: {}, rating: {}, type: {}"\
+            .format(self.user_id, self.movie_id, self.rating, self.type)
+    class Cluster(models.Model):
+        cluster_id = models.IntegerField()
+        user_id = models.IntegerField()
+
+        def __str__(self):
+            return "{} in {}".format(self.user_id, self.cluster_id)
--- myproject/analytics/views.py
+++ myproject/analytics/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
 # Create your views here.
+import json
+import decimal
+import time
+from datetime import datetime
+from django.db import connection
+
+from django.db import Count
+from django.http import  JsonResponse
+from gensim import models
+from moviegeeks.models import Movie, Genre
+from recommender.models import SeededRecs, Similarity
+
+
+def index(request):
+    context_dict = {}
+    return render(request, 'analytics/index.html', context_dict)
